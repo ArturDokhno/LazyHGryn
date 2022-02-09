717344da640a5113ad72dbd0c5c6112b96793dee
ArturDokhno <artur.dohno@icloud.com>
2022-02-09 17:10:18 +0500

    Initial Commit

---
+++ LazyHGryn/LazyHGrynApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyHGrynApp.swift
+//  LazyHGryn
+//
+//  Created by Артур Дохно on 09.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct LazyHGrynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
